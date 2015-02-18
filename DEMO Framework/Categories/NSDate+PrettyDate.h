ed79fe49b6bad0e74ef832b751d766b436141b1a
Carl Zhou <wuxi8black@gmail.com>
2015-02-18 10:47:37 -0500

    added categories

---
+++ DEMO Framework/Categories/NSDate+PrettyDate.h
@@ -0,0 +1,15 @@
+//
+//  NSDate+PrettyDate.h
+//  StyleSnaps
+//
+//  Created by carlzhou on 7/12/14.
+//  Copyright (c) 2014 Adam Dahan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (PrettyDate)
+
+- (NSString *)prettyDate;
+
+@end
