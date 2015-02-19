d15ac5bfb61065d95b2797187ed3a47795e037ca
CarlZhou <wuxi8black@gmail.com>
2015-02-19 16:45:56 -0500

    added details view

---
+++ DEMO Framework/Controllers/DetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.h
+//  DEMO Framework
+//
+//  Created by carlzhou on 2/19/15.
+//  Copyright (c) 2015 SAP Canada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Sensor;
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) Sensor *selectedSensor;
+
+@end
