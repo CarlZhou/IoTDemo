851c6bf23ca79840a4d60000c4337a60d6ef7ffc
Tracy Shi <tracyshi1217@gmail.com>
2015-03-16 16:10:38 -0400

    Alert view.

---
+++ DEMO Framework/Views/AlertView.m
@@ -0,0 +1,21 @@
+//
+//  AlertView.m
+//  DEMO Framework
+//
+//  Created by tracyshi on 2015-03-16.
+//  Copyright (c) 2015 SAP Canada. All rights reserved.
+//
+
+#import "AlertView.h"
+
+@implementation AlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
